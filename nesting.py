ff97072f9e2e6cecd3e940e6cc3d5e2de60632e1
caseycannon <ccannon@hmc.edu>
2014-01-25 09:14:58 -0800

    aaaasdf
    asdf. Reyna says this is a small project.

--- nesting.py
+++ nesting.py
@@ -4,44 +4,76 @@ from pngIO import *
 from math import *
 
 
-def newPoints((a,b), (c,d)):
-    img[a][b] = [0,0,0]
-    img[c][d] = [255,0,0]
-
+def newCorners((a,b), (c,d), img):
     l = sqrt((c-a)**2 + (d-b)**2)
     h = len(img)
-    print "h is", h
     w = len(img[0])
-    print "w is", w
     k = l * 1.0 / sqrt(w**2 + h**2)
-    print "k is", k
-    print "w**2 + h**2=", w**2 + h**2
     phi = asin(h / sqrt(w**2 + h**2)) - asin((d-b)/l)
 
-    print "phi is", phi
     x = int(a + k*w*cos(phi))
     y = int(b - k*w*sin(phi))
     v = int(c - k*w*cos(phi))
     z = int(d + k*w*sin(phi))
 
-    return (x,y,v,z)
+    return (x,y,v,z,k, phi)
 
-def interpolate(img, x, y):
+def newPoints((a,b), hDist, vDist, phi, k, img):
+    hDist = hDist - a
+    vDist = vDist - b
+    newY = hDist * sin(phi) + vDist * cos(phi)
+    newX = hDist * cos(phi) - vDist * sin(phi)
+    return (newX / k, newY / k)
+
+def interpRGB(img, x, y):
     px = x - int(x)
     py = y - int(y)
     newColor = [0,0,0]
     for color in range(3):
-        u = img[int(y)][int(x)][color] + px * (img[int(y)][int(x)+1][color])
-        v = img[int(y)+1][int(x)][color] + px * (img[int(y)+1][int(x)+1][color])
+        u = (1-px)*img[int(y)][int(x)][color] + px * (img[int(y)][int(x)+1][color])
+        v = (1-px)*img[int(y)+1][int(x)][color] + px * (img[int(y)+1][int(x)+1][color])
         newColor[color] = int(u + py *(v-u))
     return newColor
 
+def interp(y,p0,p1):
+    slope = 1.0*(p1[0] - p0[0])/(p1[1] - p0[1])
+    return p0[0] + slope * (y - p0[1])
+
+#def isWithin(x,y,a,b,c,d):
+#    if (a[0]==b[0] or a[0]==d[0]):
+#        return True
+#    count = 0
+#    if interp(y,a,b)<x: count +=1
+#    if interp(y,b,c)<x: count +=1
+#    if interp(y,c,d)<x: count +=1
+#    if interp(y,d,a)<x: count +=1
+#    return count == 2
+
+def isWithin(x,y,img):
+    return (x>=0 and y>=0 and x <= len(img[0]) - 1 and y <= len(img)-1)
+
+def nest(a, b, img):
+    (x, y, w, z, k, phi) = newCorners(a,b,img)
+    minX = min(x, w, a[0], b[0])
+    minY = min(y, z, a[1], b[1])
+    maxX = max(x, w, a[0], b[0])
+    print "y, z, a[1], b[1]", y, z, a[1], b[1]
+    maxY = max(y, z, a[1], b[1])
+
+    for c in range(minX, maxX):
+        for r in range(minY, maxY):
+            (newC, newR) = newPoints(a, c, r, k, phi, img)
+            if isWithin(newC, newR, img):
+                #print "c, r", c, r
+                img[r][c] = interpRGB(img,newC, newR)
+    #nest((x,y), (w,z), img)
+                
+
 def main():
     img = getRGB('clipboard.png')
-    h = len(img)
-    w = len(img[0])
-    fakeImg = [ [[0,1,0], [2,1,3]], [[2,1,3], [4,1,5]] ]
-    print interpolate(fakeImg,0.5,0.5)
+    nest((422,572), (100, 323), img)
+    saveRGB(img, 'result.png')
+
 
 
 if __name__ == "__main__":
